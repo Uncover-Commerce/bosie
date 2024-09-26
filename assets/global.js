3c0a3a5fa5e52490fc16fc35dcc97713245a2559
wilczu <kacpersoja1@gmail.com>
2024-09-26 15:04:51 +0100

    add formatMoney function to global.js [86c097469]

--- assets/global.js
+++ assets/global.js
@@ -6,6 +6,55 @@ function getFocusableElements(container) {
   );
 }
 
+function formatMoney(cents, format) {
+  if (typeof cents == 'string') {
+    cents = cents.replace('.', '');
+  }
+  let value = '';
+  let placeholderRegex = /\{\{\s*(\w+)\s*\}\}/;
+  let money_format = '{{ shop.money_format }}';
+  let formatString = format || money_format;
+
+  function defaultOption(opt, def) {
+    return typeof opt == 'undefined' ? def : opt;
+  }
+
+  function formatWithDelimiters(number, precision, thousands, decimal) {
+    precision = defaultOption(precision, 2);
+    thousands = defaultOption(thousands, ',');
+    decimal = defaultOption(decimal, '.');
+
+    if (isNaN(number) || number == null) {
+      return 0;
+    }
+
+    number = (number / 100.0).toFixed(precision);
+
+    let parts = number.split('.'),
+      dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + thousands),
+      cents = parts[1] ? decimal + parts[1] : '';
+
+    return dollars + cents;
+  }
+
+  switch (formatString.match(placeholderRegex)[1]) {
+    case 'amount':
+      value = formatWithDelimiters(cents, 2);
+      break;
+    case 'amount_no_decimals':
+      value = formatWithDelimiters(cents, 0);
+      break;
+    case 'amount_with_comma_separator':
+      value = formatWithDelimiters(cents, 2, '.', ',');
+      break;
+    case 'amount_no_decimals_with_comma_separator':
+      value = formatWithDelimiters(cents, 0, '.', ',');
+      break;
+  }
+
+  return formatString.replace(placeholderRegex, value);
+}
+
 class SectionId {
   static #separator = '__';
 
@@ -1267,21 +1316,19 @@ if (!customElements.get('bulk-add')) {
   customElements.define('bulk-add', BulkAdd);
 }
 
-
-
-// Mega Menu 
-document.addEventListener('DOMContentLoaded', function() {
+// Mega Menu
+document.addEventListener('DOMContentLoaded', function () {
   const menuItems = document.querySelectorAll('.menu-item.has-dropdown');
 
-  menuItems.forEach(item => {
-    item.addEventListener('mouseenter', function() {
+  menuItems.forEach((item) => {
+    item.addEventListener('mouseenter', function () {
       const megaMenu = this.querySelector('.mega-menu');
       if (megaMenu) {
         megaMenu.style.display = 'block';
       }
     });
 
-    item.addEventListener('mouseleave', function() {
+    item.addEventListener('mouseleave', function () {
       const megaMenu = this.querySelector('.mega-menu');
       if (megaMenu) {
         megaMenu.style.display = 'none';
